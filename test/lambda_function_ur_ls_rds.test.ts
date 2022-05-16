54def4efd4831c3be66be98bd414f6d608c9f269
matsuda yuki <yiyth.fcb6@gmail.com>
2022-05-16 21:01:02 +0900

    Initial commit

---
+++ test/lambda_function_ur_ls_rds.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LambdaFunctionUrLsRds from '../lib/lambda_function_ur_ls_rds-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lambda_function_ur_ls_rds-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LambdaFunctionUrLsRds.LambdaFunctionUrLsRdsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
